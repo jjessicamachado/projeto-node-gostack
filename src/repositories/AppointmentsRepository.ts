04d7397605a3b83be0a42d18ecab061b14d8e71d
Jéssica Machado <jessicasmachado@live.com>
2020-09-08 19:55:54 -0300

    Conceito de DTO

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,11 +1,20 @@
 import Appointment from '../models/Appointment';
 import { isEqual } from 'date-fns';
 
+interface createAppointmentDTO {
+    provider: string;
+    date: Date
+}
+
 class AppointmentsRepository {
     private appointments: Appointment[];
 
     constructor() {
         this.appointments = [];
+    }
+
+    public all(): Appointment[] {
+        return this.appointments;
     }
 
     public findByDate(date: Date): Appointment | null {
@@ -15,8 +24,8 @@ class AppointmentsRepository {
         return findAppointment || null;
     }
 
-    public create(provider: string, date: Date): Appointment {
-        const appointment = new Appointment(provider, date);
+    public create({ provider, date }: createAppointmentDTO): Appointment {
+        const appointment = new Appointment({ provider, date });
 
         this.appointments.push(appointment);
 
